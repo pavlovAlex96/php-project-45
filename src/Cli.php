c306c28515d645e05b0e33cb380af533706bec80
AlexLenovo <pavlov.aleksandr.1196@gmail.com>
2026-02-26 23:55:29 +0500

    дописана логика игры "Проверка на чётность"

--- src/Cli.php
+++ src/Cli.php
@@ -16,5 +16,24 @@ function greetings()
 //  Правильный ответ, проверка четности
 function parityCheck()
 {
-    $ee;
+    $i = 0;
+    do {
+        $gen_number = rand(1, 100);
+        $correct_answer = (($gen_number % 2) == 0) ? 'yes' : 'no';
+        line("Question: %s", $gen_number);
+        $usr_answer = prompt('Your answer: ');
+
+        if ($usr_answer === $correct_answer) {
+            $i++;
+            line('Correct!');
+        } else {
+            line("'$usr_answer' is wrong answer ;(. Correct answer was '$correct_answer'. \nLet's try again, Tirion!");
+            break;
+        }
+    } while ($i < 3);
+
+    if ($i == 3) {
+        //Первая игра пройдена
+        line('Congratulations, Tirion!');
+    }
 }
