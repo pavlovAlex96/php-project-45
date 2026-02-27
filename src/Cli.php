680a4a9bdd7d6a5777ee896d44a8a10aaec5ba40
AlexLenovo <pavlov.aleksandr.1196@gmail.com>
2026-02-27 22:29:05 +0500

    перенос логики игры в новый исполняемый файл brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -12,28 +12,3 @@ function greetings()
     $name = prompt('May I have your Name?');
     line("Hello, %s", $name);
 }
-
-//  Правильный ответ, проверка четности
-function parityCheck()
-{
-    $i = 0;
-    do {
-        $gen_number = rand(1, 100);
-        $correct_answer = (($gen_number % 2) == 0) ? 'yes' : 'no';
-        line("Question: %s", $gen_number);
-        $usr_answer = prompt('Your answer: ');
-
-        if ($usr_answer === $correct_answer) {
-            $i++;
-            line('Correct!');
-        } else {
-            line("'$usr_answer' is wrong answer ;(. Correct answer was '$correct_answer'. \nLet's try again, Tirion!");
-            break;
-        }
-    } while ($i < 3);
-
-    if ($i == 3) {
-        //Первая игра пройдена
-        line('Congratulations, Tirion!');
-    }
-}
