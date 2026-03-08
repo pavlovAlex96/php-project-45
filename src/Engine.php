647a244dd409e44d7143a72865e5d894fc9363b6
Pavlov <pavlov.aleksandr.1196@gmail.com>
2026-03-08 11:47:36 +0500

    Указание типа переменных в функциях

--- src/Engine.php
+++ src/Engine.php
@@ -6,18 +6,18 @@ use function cli\line;
 use function cli\prompt;
 
 //  Устанавливаем колличество раундов в играх
-function setRound()
+function setRound(): int
 {
     return 3;
 }
 
 //  Проверка ответа пользователя с решением
-function askAnswer($task)
+function askAnswer(array $task): bool
 {
     line("Question: %s", $task['question']);
     $usrAnswer = prompt('Your answer: ');
 
-    if ($usrAnswer == $task['answer']) {
+    if ($usrAnswer === $task['answer']) {
         line('Correct!');
         return true;
     } else {
@@ -28,8 +28,9 @@ function askAnswer($task)
 }
 
 // Генирация прогрессии
-function progressionCreating($start, $step, $light)
+function progressionCreating(int $start, int $step, int $light): array 
 {
+    $progression = [];
     for ($index = 0; $index <= $light; $index++) {
         $progression[$index] = $start + $step * $index;
     }
