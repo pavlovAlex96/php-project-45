2b23be566c633a127302c4b98797b447127bde25
AlexLenovo <pavlov.aleksandr.1196@gmail.com>
2026-03-12 09:48:40 +0500

    исправлена ошибка в функци getTaskPrime

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -13,22 +13,21 @@ function run(): void
 
 
 function getTaskPrime(): array
-{
+{    
     $task = ['question' => rand(2, 100)];
 
-    if ($task['question'] = 2 || 3) {
+    if ($task['question'] == 2 || $task['question'] == 3) {
         $task['answer'] = 'yes';
     } elseif ($task['question'] % 2 == 0) {
         $task['answer'] = 'no';
     } else {
         $sqrtRoundQuest = round(sqrt($task['question']));
+        $task['answer'] = 'yes';
         for ($num = 3; $num <= $sqrtRoundQuest; $num += 2) {
             if ($task['question'] % $num == 0) {
                 $task['answer'] = 'no';
                 break;
-            } else {
-                $task['answer'] = 'yes';
-            }
+            } 
         }
     }
     return $task;
