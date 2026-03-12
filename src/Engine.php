6cef00a6dcac99e0ccf9c2f72ab981f814bc6ea7
AlexLenovo <pavlov.aleksandr.1196@gmail.com>
2026-03-12 08:50:12 +0500

    Добавлен весь общий фукционал

--- src/Engine.php
+++ src/Engine.php
@@ -1,37 +1,42 @@
 <?php
 
-namespace Hexlet\Code;
+namespace Hexlet\Code\Engine;
 
 use function cli\line;
 use function cli\prompt;
+use function Hexlet\Code\Games\Calc\getTaskCalc;
+use function Hexlet\Code\Games\Even\getTaskEven;
+use function Hexlet\Code\Games\Gcd\getTaskGcd;
+use function Hexlet\Code\Games\Prime\getTaskPrime;
+use function Hexlet\Code\Games\Progression\getTaskProgression;
 
 //  Устанавливаем колличество раундов в играх
-function setRound(): int
-{
-    return 3;
-}
+const ROUND = 3;
 
 //  Проверка ответа пользователя с решением
-function askAnswer(array $task, string $name): bool
+function runGame(callable $getTask, string $rules): void
 {
-    line("Question: %s", $task['question']);
-    $usrAnswer = prompt('Your answer: ');
+    //  Приветствие
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($rules);
 
-    if ($usrAnswer === (string)$task['answer']) {
-        line('Correct!');
-        return true;
-    } else {
-        line("'%s' is wrong answer ;(. Correct answer was '%s'.", $usrAnswer, $task['answer']);
-        line("Let's try again, %s!", $name);
-        return false;
-    }
-}
+    for ($numdTrueAnswer = 1; $numdTrueAnswer <= ROUND; $numdTrueAnswer++) {
+        $task = $getTask();
+        line("Question: %s", $task['question']);
+        $usrAnswer = prompt('Your answer: ');
 
-//  Поздравление при полном прохождении
-function congratulations(int $nuber, int $finalNumber, string $user): void
-{
-    //  Игра пройдена
-    if ($nuber === $finalNumber) {
-        line('Congratulations, %s!', $user);
+        if ($usrAnswer === (string)$task['answer']) {
+            line('Correct!');
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $usrAnswer, $task['answer']);
+            line("Let's try again, %s!", $name);
+            break;
+        }
+        //  Игра пройдена
+        if ($numdTrueAnswer === ROUND) {
+            line('Congratulations, %s!', $name);
+        }
     }
 }
